73242f0ce1092441e3e0399081655710b0abcd84
Jasurlv <jasurjuraev.lv@gmail.com>
2025-08-23 01:54:05 -0700

    Update test_visa.py

--- test_visa.py
+++ test_visa.py
@@ -6,21 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+CHAT_IDS = os.getenv("CHAT_IDS").split(",")
 
 def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message,
-        "parse_mode": "HTML"
-    }
-    response = requests.post(url, data=payload)
-    
-    if response.status_code == 200:
-        print("Message sent successfully!")
-    else:
-        print(f"Failed to send message: {response.status_code} - {response.text}")
+    for chat_id in CHAT_IDS:
+        payload = {
+            "chat_id": chat_id.strip(),
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print(f"Message sent successfully to {chat_id}!")
+        else:
+            print(f"Failed to send to {chat_id}: {response.status_code} - {response.text}")
 
 def go_to_calendar(page: Page):
     """Fill form and navigate to calendar page"""
